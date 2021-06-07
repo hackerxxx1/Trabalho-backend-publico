b56d91edc40c06f5d5e8730c2a82bc018e742a38
hackerxxx1 <43994798+hackerxxx1@users.noreply.github.com>
2021-06-07 15:06:06 -0300

    alteração altenticação

---
+++ app/Controllers/Http/UserController.js
@@ -0,0 +1,27 @@
+'use strict'
+
+const User = use('App/Models/User')
+
+class UserController {
+
+  async index(){
+    return User.all()
+  }
+
+  async store({request}){
+    const camposCadastro = ['username','email','password']
+    const dados = request.only(camposCadastro)
+
+    return await User.create(dados)
+  }
+
+  async token({request, auth}){
+
+    const {email, password} = request.all()
+    return await auth.attempt(email, password)
+
+  }
+
+}
+
+module.exports = UserController
